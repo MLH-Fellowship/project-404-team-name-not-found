4e547dabf26198f37472f6889182d2120bb2ea42
Andres <67043785+GuppyPaw@users.noreply.github.com>
2022-07-18 15:35:45 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-tmux kill-server
 cd project-404-team-name-not-found
 git fetch && git reset origin/main --hard
 . python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -d 'source project-404-team-name-not-found | source python3-virtualenv/bin/activate | flask run --host=0.0.0.0'
+systemctl restart myportfolio
